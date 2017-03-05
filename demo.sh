221489d475b2ee965db7bd50adc236c83bfdd135
Jackie Meng <mengjq08@gmail.com>
2017-03-05 10:11:53 +0800

    add demo command

---
+++ demo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+gulp
+rm -rf ../dist
+mv dist ../
+
+git checkout gh-pages
+rm -rf $(ls dist | grep -v fonts)
+cp -R ../dist ./
+git add .
+git commit -m "auto update"
+git push origin master
+
+rm -rf ../dist
+git checkout master
